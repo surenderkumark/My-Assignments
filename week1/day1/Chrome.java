b395178d60663aaea3dd459e4f0d8f3643d80f29
surenderkumark <surenderkumar2393@gmail.com>
2025-07-22 23:21:01 +0530

    Add files via upload
    Week1day1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float a=91.0f;
+		System.out.println("version of type float is"+" "+ a);
+		
+		String b="Google";
+		System.out.println("developer of type String is"+" "+ b);
+		
+		boolean beta= false;
+		System.out.println("isBeta of type boolean "+" "+ beta);
+					
+				
+		int d=2008;
+		System.out.println("releaseYear of type is"+" "+ d);
+		
+		char e='C';
+		System.out.println("shortcutKey of type char is"+" "+ e);
+		
+	}
+
+		}
